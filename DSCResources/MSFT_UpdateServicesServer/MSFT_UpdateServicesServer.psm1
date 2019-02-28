8b3c1636f5647a2b39b48f987efa28ba4a4caf8d
gaelcolas <gaelcolas@synedgy.com>
2019-02-28 14:42:32 +0800

    fixing PSSA rules for MSFT_UpdateServicesServer

--- DSCResources/MSFT_UpdateServicesServer/MSFT_UpdateServicesServer.psm1
+++ DSCResources/MSFT_UpdateServicesServer/MSFT_UpdateServicesServer.psm1
@@ -128,7 +128,8 @@ function Get-TargetResource
         Write-Verbose -Message "WSUSServer languages are $Languages"
 
         Write-Verbose -Message 'Getting WSUSServer classifications'
-        if ($Classifications = @($WsusSubscription.GetUpdateClassifications().ID.Guid)) {
+        if ($Classifications = @($WsusSubscription.GetUpdateClassifications().ID.Guid))
+        {
             if($null -eq (Compare-Object -ReferenceObject ($Classifications | Sort-Object -Unique) -DifferenceObject `
                     (($WsusServer.GetUpdateClassifications().ID.Guid) | Sort-Object -Unique) -SyncWindow 0))
             {
@@ -140,7 +141,8 @@ function Get-TargetResource
         }
         Write-Verbose -Message "WSUSServer classifications are $Classifications"
         Write-Verbose -Message 'Getting WSUSServer products'
-        if ($Products = @($WsusSubscription.GetUpdateCategories().Title)) {
+        if ($Products = @($WsusSubscription.GetUpdateCategories().Title))
+        {
             if($null -eq (Compare-Object -ReferenceObject ($Products | Sort-Object -Unique) -DifferenceObject `
                     (($WsusServer.GetUpdateCategories().Title) | Sort-Object -Unique) -SyncWindow 0))
             {
@@ -322,6 +324,7 @@ function Set-TargetResource
         [System.Boolean]
         $Synchronize,
 
+        [Parameter()]
         [ValidateSet("Client", "Server")]
         [System.String]
         $ClientTargetingMode
@@ -768,6 +771,7 @@ function Test-TargetResource
         [System.Boolean]
         $Synchronize,
 
+        [Parameter()]
         [ValidateSet("Client", "Server")]
         [System.String]
         $ClientTargetingMode
