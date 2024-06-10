9d4e7c1df404382029bc864dafe415bfe6daae0f
Johan Ljunggren <johlju@users.noreply.github.com>
2024-06-10 16:13:38 +0200

    UpdateServiceDsc: Update localization strings (#79)

--- source/DSCResources/MSFT_UpdateServicesServer/MSFT_UpdateServicesServer.psm1
+++ source/DSCResources/MSFT_UpdateServicesServer/MSFT_UpdateServicesServer.psm1
@@ -18,7 +18,8 @@
 # Load Common Module
 $script:resourceHelperModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\DscResource.Common'
 Import-Module -Name $script:resourceHelperModulePath
-$script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US' -FileName 'MSFT_UpdateServicesServer.strings.psd1'
+
+$script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
 
 <#
     .SYNOPSIS
