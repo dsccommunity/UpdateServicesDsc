f150c6dd2b8b655b38d878e554523ccc57b74a64
Daniel Hughes <2237515+dan-hughes@users.noreply.github.com>
2025-10-10 14:20:29 +0100

    Add wildcard helper

--- tests/Unit/TestHelpers/CommonTestHelper.psm1
+++ tests/Unit/TestHelpers/CommonTestHelper.psm1
@@ -153,3 +153,86 @@ function Get-WsusServerTemplate
 
     return $WsusServer
 }
+
+function Get-WsusServerMockWildCardPrdt
+{
+    $wsusServer = Get-WsusServerTemplate
+
+    # Override GetSubscription method
+    $WsusServer | Add-Member -Force -MemberType ScriptMethod -Name GetSubscription -Value {
+        $Subscription = [PSCustomObject] @{
+            SynchronizeAutomaticallyTimeOfDay = '04:00:00'
+            NumberOfSynchronizationsPerDay = 24
+            SynchronizeAutomatically = $true
+        }
+
+        $Subscription | Add-Member -MemberType ScriptMethod -Name StartSynchronization -Value {}
+        $Subscription | Add-Member -MemberType ScriptMethod -Name GetUpdateClassifications -Value {
+            $UpdateClassification = [PSCustomObject] @{
+                Name = 'Update Classification'
+                ID = [PSCustomObject] @{
+                    GUID = '00000000-0000-0000-0000-0000testguid'
+                }
+            }
+
+            return $UpdateClassification
+        }
+
+        $Subscription | Add-Member -Force -MemberType ScriptMethod -Name GetUpdateCategories -Value {
+            $Categories = [PSCustomObject] @{
+                Title = 'Windows Server 2003'
+            },
+            [PSCustomObject] @{
+                Title = 'Windows Server 2008'
+            },
+            [PSCustomObject] @{
+                Title = 'Windows Server 2008R2'
+            },
+            [PSCustomObject] @{
+                Title = 'Windows Server 2012'
+            },
+            [PSCustomObject] @{
+                Title = 'Windows Server 2016'
+            },
+            [PSCustomObject] @{
+                Title = 'Windows Server 2019'
+            }
+
+            return $Categories
+        }
+
+        return $Subscription
+    }
+
+    # Override GetUpdateCategories method
+    $WsusServer | Add-Member -Force -MemberType ScriptMethod -Name GetUpdateCategories -Value {
+        $Categories = [PSCustomObject] @{
+            Title = 'Windows'
+        },
+        [PSCustomObject] @{
+            Title = 'Office'
+        },
+        [PSCustomObject] @{
+            Title = 'Windows Server 2003'
+        },
+        [PSCustomObject] @{
+            Title = 'Windows Server 2008'
+        },
+        [PSCustomObject] @{
+            Title = 'Windows Server 2008R2'
+        },
+        [PSCustomObject] @{
+            Title = 'Windows Server 2012'
+        },
+        [PSCustomObject] @{
+            Title = 'Windows Server 2016'
+        },
+        [PSCustomObject] @{
+            Title = 'Windows Server 2019'
+        }
+
+        return $Categories
+    }
+
+    return $WsusServer
+}
